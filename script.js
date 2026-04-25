e89764d44ef580a46d3749f5cc3a2dde4ebdad21
Blu_Axolotl_YT <112743487+cogf@users.noreply.github.com>
2026-04-25 21:58:27 +0530

    Delete script.jsqfw

--- script.js
+++
@@ -1,9 +0,0 @@
-function openStory(title,text){
-document.getElementById('storyTitle').textContent=title;
-document.getElementById('storyText').textContent=text;
-document.getElementById('overlay').classList.remove('hidden');
-}
-
-function closeStory(){
-document.getElementById('overlay').classList.add('hidden');
-}
