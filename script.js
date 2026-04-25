ddb852840130d6ac0e4800c94e5ff763679a8c26
Blu_Axolotl_YT <112743487+cogf@users.noreply.github.com>
2026-04-25 22:56:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ for(let i=1;i<=30;i++){
 let option=document.createElement('option');
 option.value=i;
 option.textContent='Theme '+i;
-document.getElementById('themePicker').appendChild(option);
+themePicker.appendChild(option);
 }
 
-document.getElementById('themePicker').addEventListener('change',function(){
+themePicker.addEventListener('change',function(){
 document.body.className='theme'+this.value;
 });
 
 function openStory(title,text){
-document.getElementById('title').innerText=title;
-document.getElementById('content').innerText=text;
-document.getElementById('popup').classList.remove('hidden');
+titleEl.innerText=title;
+content.innerText=text;
+popup.classList.remove('hidden');
 }
 
 function closeStory(){
-document.getElementById('popup').classList.add('hidden');
+popup.classList.add('hidden');
 }
