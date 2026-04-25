c9938d4d1cb584bde4bcb2c26f163e3be62bed98
Blu_Axolotl_YT <112743487+cogf@users.noreply.github.com>
2026-04-25 21:54:05 +0530

    few

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function openStory(title,text){
 document.getElementById('storyTitle').textContent=title;
 document.getElementById('storyText').textContent=text;
-document.getElementById('popup').classList.remove('hidden');
+document.getElementById('overlay').classList.remove('hidden');
 }
 
 function closeStory(){
-document.getElementById('popup').classList.add('hidden');
+document.getElementById('overlay').classList.add('hidden');
 }
